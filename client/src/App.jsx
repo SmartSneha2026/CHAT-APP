4a61354a62befed05dd8200ce96010f7ab704bcb
SmartSneha2026 <snehaprajapti2456@gmail.com>
2026-01-16 22:25:23 +0530

    App.js updateion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import React, { use, useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getUser } from './store/slices/authSlice';
-import { connectSocket } from './lib/socket';
+import { getUser, setOnlineUsers } from './store/slices/authSlice';
+import { connectSocket, disconnectSocket } from './lib/socket';
+import {BrowserRouter as Router , Routes, Route, Navigate} from 'react-router-dom'
+import { Loader } from 'lucide-react';
 const App = () => {
   
   const {authUser, ischeckingAuth} = useSelector((state) => state.auth);
@@ -13,9 +15,19 @@ const App = () => {
   useEffect(() => {
     if(authUser) {
       const socket = connectSocket(authUser._id);
-
-      
-    }
+       
+        socket.on('getOnlineUsers', (users) => {
+           dispatch(setOnlineUsers(users));
+        });
+        return () => disconnectSocket();
+      }
+      if(ischeckingAuth && !authUser) {
+        return (
+          <div className = "flex items-center justify-center h-screen">
+            <Loader className='size-20 animate-spin' />
+          </div>
+        )
+      }
   }, [authUser]);
   return (
     <></>
